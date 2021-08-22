cc6129572ad342e799a3ebe57ad4be522df7904e
ChijiokeOkoro <67354170+ChijiokeOkoro@users.noreply.github.com>
2021-08-22 18:57:56 -0400

    Update Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -7,12 +7,13 @@ public class Deck {
 	public Card deck[] = new Card[52];
 	int topDeck = 0;
 	
-	
+	// creates a deck of 52 cards and shuffles the deck using the shuffle method
 	public Deck() {
 		for (int i = 0; i < deck.length; i++) {
 			deck[i] = new Card(i % 13, i / 13);
 		}
 		
+		// "shuffles" 10 times
 		for (int i = 0; i < 10; i++) {
 			shuffle();
 		}
@@ -21,6 +22,7 @@ public class Deck {
 		
 	}
 	
+	// method "shuffles" by swapping the location of each card in the deck with a random value
 	public void shuffle() {
 		for (int i = 0; i < deck.length; i++){
 			int j = new Random().nextInt(52);
@@ -30,6 +32,7 @@ public class Deck {
 		}
 	}
 	
+	// creates a visual of the deck on the screen
 	public void draw(Graphics2D g) {
 		g.setColor(Color.red);
 		g.fillRect(550, 295, 100, 150);
@@ -44,6 +47,7 @@ public class Deck {
 		}
 	}
 	
+	// shows each card in a players hand 
 	public void drawCard(Player player) {
 		if(player.totalCards < 5) {
 			player.hand[player.totalCards] = deck[topDeck];
