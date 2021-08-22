07ec0b76d9b014da08694ee96b647321e9aa0dbb
ChijiokeOkoro <67354170+ChijiokeOkoro@users.noreply.github.com>
2021-08-22 18:57:50 -0400

    Update BlackJack_Main.java

--- src/BlackJack_Main.java
+++ src/BlackJack_Main.java
@@ -3,16 +3,18 @@ import javax.swing.JFrame;
 public class BlackJack_Main {
 	public static void main(String[] args) {
 		
+		// adds the game onto the screen
 		JFrame blackjack = new JFrame();
 		GameRunner game = new GameRunner();
 		blackjack.add(game);
 		
+		//bounds for the screen of teh game 
 		blackjack.setBounds(1,1,1200,720);
 		
 		
 		blackjack.setTitle("Blackjack");
 		
-		
+		//Makes the screen visible 
 		blackjack.setResizable(false);
 		blackjack.setVisible(true);
 		blackjack.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
